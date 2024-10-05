3d32273d7a9e9511ae4da21f1ab8e7ee68874dbc
joel-jcs <joeljcs@protonmail.com>
2024-10-04 22:46:25 -0400

    added testing for hit and isSunk functions

--- src/Ship.test.js
+++ src/Ship.test.js
@@ -1,10 +1,75 @@
-const Ship = require('./Ship');
+const ShipHandler = require('./Ship');
 
-test('Ship', () => {
-  const ship = Ship.createShip();
-  expect(ship).toEqual({
-    length: null,
-    timesHit: null,
+beforeAll(() => {
+  carrier1 = ShipHandler.createShip(5);
+  battleship1 = ShipHandler.createShip(4);
+  destroyer1 = ShipHandler.createShip(3);
+  submarine1 = ShipHandler.createShip(3);
+  patrol1 = ShipHandler.createShip(2);
+});
+
+test('Ship constructor', () => {
+  expect(carrier1).toEqual({
+    length: 5,
+    timesHit: 0,
     isSunk: false,
+  });
+
+  expect(patrol1).toEqual({
+    length: 2,
+    timesHit: 0,
+    isSunk: false,
+  });
+});
+
+describe('hit function', () => {
+  afterEach(() => {
+    carrier1 = ShipHandler.createShip(5);
+    battleship1 = ShipHandler.createShip(4);
+    destroyer1 = ShipHandler.createShip(3);
+    submarine1 = ShipHandler.createShip(3);
+    patrol1 = ShipHandler.createShip(2);
+  });
+  test('function should increment the ships hit counter', () => {
+    console.log(ShipHandler.hit(carrier1));
+    expect(carrier1.timesHit).toBe(1);
+  });
+
+  test('increment ship hits separately', () => {
+    carrier2 = ShipHandler.createShip(5);
+    battleship2 = ShipHandler.createShip(4);
+    destroyer2 = ShipHandler.createShip(3);
+    submarine2 = ShipHandler.createShip(3);
+    patrol2 = ShipHandler.createShip(2);
+
+    ShipHandler.hit(battleship1);
+    ShipHandler.hit(battleship2);
+    ShipHandler.hit(battleship2);
+
+    expect(battleship1.timesHit).toBe(1);
+    expect(battleship2.timesHit).toBe(2);
+  });
+});
+
+describe('isSunk function', () => {
+  test('return true if timesHit = ship.length', () => {
+    carrier1.timesHit = 5;
+    expect(ShipHandler.isSunk(carrier1)).toBe(true);
+  });
+
+  test('return false if timesHit =/= ship.length', () => {
+    carrier1.timesHit = 3;
+    expect(ShipHandler.isSunk(carrier1)).toBe(false);
+
+    patrol1.timesHit = 0;
+    expect(ShipHandler.isSunk(patrol1)).toBe(false);
+  });
+
+  test('return true if timesHit > ship.length', () => {
+    battleship1.timesHit = 6;
+    expect(ShipHandler.isSunk(battleship1)).toBe(true);
+
+    submarine1.timesHit = 0;
+    expect(ShipHandler.isSunk(submarine1)).toBe(false);
   });
 });
