9246d76b6dd4e4e78757d3e2016647954aa171ca
joel-jcs <joeljcs@protonmail.com>
2024-10-06 13:52:16 -0400

    added gameboard rendering and styles

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -2,6 +2,56 @@ import GameboardHandler from './Gameboard';
 import PlayerHandler from './Player';
 import ShipHandler from './Ship';
 
-const DOMHandler = {};
+const DOMHandler = () => {
+  const renderSettings = () => {
+    const settingsContainer = document.getElementById('settings-container');
 
-export default DOMHandler;
+    return settingsContainer;
+  };
+
+  const renderGameboards = () => {
+    const player1Container = document.getElementById('player1-container');
+    const player2Container = document.getElementById('player2-container');
+
+    const player1Grid = document.createElement('div');
+    const player2Grid = document.createElement('div');
+
+    player1Grid.id = 'player1-grid';
+    player2Grid.id = 'player2-grid';
+
+    player1Grid.classList.add('gameboard');
+    player2Grid.classList.add('gameboard');
+
+    player1Container.appendChild(player1Grid);
+    player2Container.appendChild(player2Grid);
+
+    const player1Board = GameboardHandler.createGameboard();
+    player1Board.grid.forEach((row) => {
+      // eslint-disable-next-line no-unused-vars
+      row.forEach((col) => {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        player1Grid.appendChild(cell);
+      });
+    });
+
+    const player2Board = GameboardHandler.createGameboard();
+    player2Board.grid.forEach((row) => {
+      // eslint-disable-next-line no-unused-vars
+      row.forEach((col) => {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        player2Grid.appendChild(cell);
+      });
+    });
+
+    return { player1Container, player2Container };
+  };
+
+  return {
+    renderSettings,
+    renderGameboards,
+  };
+};
+
+export default DOMHandler();
