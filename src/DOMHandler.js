f01e3f2066c88cfefecd783d2d969fe9e23805fb
joel-jcs <joeljcs@protonmail.com>
2024-10-06 14:28:25 -0400

    added grid labels and styling

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -13,6 +13,40 @@ const DOMHandler = () => {
     const player1Container = document.getElementById('player1-container');
     const player2Container = document.getElementById('player2-container');
 
+    const gridRowLegend = document.createElement('div');
+    const gridColLegend = document.createElement('div');
+
+    gridRowLegend.classList.add('grid-row-legend');
+    gridColLegend.classList.add('grid-col-legend');
+
+    const gridRowLabelArr = [];
+    const gridColLabelArr = [];
+    for (let i = 0; i < 10; i++) {
+      gridRowLabelArr.push(String.fromCharCode(i + 65));
+      gridColLabelArr.push(i + 1);
+    }
+
+    gridRowLabelArr.forEach((row) => {
+      const gridRowLabel = document.createElement('div');
+      gridRowLabel.classList.add('grid-row-labels');
+      gridRowLabel.textContent = row;
+      gridRowLegend.append(gridRowLabel);
+    });
+
+    gridColLabelArr.forEach((col) => {
+      const gridColLabel = document.createElement('div');
+      gridColLabel.classList.add('grid-col-labels');
+      gridColLabel.textContent = col;
+      gridColLegend.append(gridColLabel);
+    });
+
+    const gridRowLegend2 = gridRowLegend.cloneNode(true);
+    const gridColLegend2 = gridColLegend.cloneNode(true);
+    player1Container.append(gridRowLegend);
+    player1Container.append(gridColLegend);
+    player2Container.append(gridRowLegend2);
+    player2Container.append(gridColLegend2);
+
     const player1Grid = document.createElement('div');
     const player2Grid = document.createElement('div');
 
@@ -22,8 +56,8 @@ const DOMHandler = () => {
     player1Grid.classList.add('gameboard');
     player2Grid.classList.add('gameboard');
 
-    player1Container.appendChild(player1Grid);
-    player2Container.appendChild(player2Grid);
+    player1Container.append(player1Grid);
+    player2Container.append(player2Grid);
 
     const player1Board = GameboardHandler.createGameboard();
     player1Board.grid.forEach((row) => {
@@ -31,7 +65,7 @@ const DOMHandler = () => {
       row.forEach((col) => {
         const cell = document.createElement('div');
         cell.classList.add('cell');
-        player1Grid.appendChild(cell);
+        player1Grid.append(cell);
       });
     });
 
@@ -41,12 +75,14 @@ const DOMHandler = () => {
       row.forEach((col) => {
         const cell = document.createElement('div');
         cell.classList.add('cell');
-        player2Grid.appendChild(cell);
+        player2Grid.append(cell);
       });
     });
 
     return { player1Container, player2Container };
   };
+
+  const renderShips = (gameboard) => {};
 
   return {
     renderSettings,
