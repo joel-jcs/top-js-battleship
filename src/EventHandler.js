d24861384c5729dbd6b78d3081b37e7d0765c324
joel-jcs <joeljcs@protonmail.com>
2024-10-06 21:44:55 -0400

    feat: added attackListener

--- src/EventHandler.js
+++ src/EventHandler.js
@@ -1,7 +1,37 @@
 import GameboardHandler from './Gameboard';
 import PlayerHandler from './Player';
 import ShipHandler from './Ship';
+import DOMHandler from './DOMHandler';
 
-const EventHandler = {};
+const EventHandler = () => {
+  const attackListener = (player, opponentGrid, opponentBoard) => {
+    console.log(opponentGrid);
 
-export default EventHandler;
+    if (player.isCPU === false) {
+      const gridRows = opponentGrid.querySelectorAll('.row');
+
+      gridRows.forEach((row, rowIndex) => {
+        const columns = row.querySelectorAll('.cell');
+
+        columns.forEach((column, colIndex) => {
+          column.addEventListener('click', (event) => {
+            const { target } = event;
+            console.log(target, rowIndex, colIndex);
+
+            const attackedBoard = PlayerHandler.attack(opponentBoard, {
+              row: rowIndex,
+              col: colIndex,
+            });
+
+            DOMHandler.updateBoard(attackedBoard, opponentGrid);
+          });
+        });
+      });
+    }
+  };
+  return {
+    attackListener,
+  };
+};
+
+export default EventHandler();
