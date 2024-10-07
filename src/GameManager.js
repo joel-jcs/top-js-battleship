88116f20d6deafdadf438d7f39c1c4e6fea1e79e
joel-jcs <joeljcs@protonmail.com>
2024-10-06 21:44:02 -0400

    init commit

---
+++ src/GameManager.js
@@ -0,0 +1,7 @@
+import GameboardHandler from './Gameboard';
+import PlayerHandler from './Player';
+import ShipHandler from './Ship';
+import EventHandler from './EventHandler';
+
+const GameManager = () => {};
+export default GameManager;
