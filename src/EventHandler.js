d1056287ce686cec5ae2e69019fb38bff29d00f6
joel-jcs <joeljcs@protonmail.com>
2024-10-07 21:31:57 -0400

    added listeners for the start game and restart game buttons

--- src/EventHandler.js
+++ src/EventHandler.js
@@ -3,6 +3,20 @@ import DOMHandler from './DOMHandler';
 import GameManager from './GameManager';
 
 const EventHandler = () => {
+  const startGameListener = (player1, player2) => {
+    const startBtn = document.getElementById('start-btn');
+    startBtn.addEventListener('click', () => {
+      const player1Container = document.getElementById('player1-container');
+      const player2Container = document.getElementById('player2-container');
+      player1Container.style.opacity = '1';
+      player2Container.style.opacity = '1';
+
+      startBtn.remove();
+      DOMHandler.renderInfoMessage();
+      GameManager.startTurn(player1, player2);
+    });
+  };
+
   const attackListener = (player, opponent, opponentGrid) => {
     const opponentBoard = opponent.gameboard;
 
@@ -37,8 +51,17 @@ const EventHandler = () => {
     };
     opponentGrid.addEventListener('mouseup', clickHandler);
   };
+
+  const restartGameListener = () => {
+    const restartBtn = document.getElementById('restart-btn');
+    restartBtn.addEventListener('click', () => {
+      window.location.reload();
+    });
+  };
   return {
+    startGameListener,
     attackListener,
+    restartGameListener,
   };
 };
 
